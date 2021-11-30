9ed87db08eeaf8dcd4479a1ab780019d07e566b5
PrestonB404 <94154598+PrestonB404@users.noreply.github.com>
2021-11-29 22:28:44 -0800

    Added Mobile App Code
    Giahuy Gonzalez's code.

---
+++ ERIS.MobileApp/CalTrans/CalTrans/LoginUI.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalTrans
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+
+        }
+
+        private String server = "Server=120.0.0.1;User ID=yowzal;Password=1109;Database=sakila";
+        //protected override void OnAppearing()
+        //{
+         //   MySqlConnection sql = new MySqlConnection(server);
+          //  sql.Open();
+           // sql.Close();
+        //}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            MySqlConnection sql = new MySqlConnection(server);
+            sql.Open();
+
+            MySqlCommand cmd = new MySqlCommand("Insert into users (first_name,last_name) values('" + txtUsername.Text + "','" + txtPassword.Text + "')", sql);
+            var rd = cmd.ExecuteReaderAsync();
+
+            sql.Close();
+
+            DisplayAlert("Info", "Data something", "OK");
+            txtUsername.Text = null;
+            txtPassword.Text = null;
+
+
+            if(txtUsername.Text=="admin" && txtPassword.Text == "123")
+            {
+
+                Navigation.PushAsync(new Home());
+            }else
+            {
+                DisplayAlert("Oopsies...", "Username or Password is incorrect!", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+
+        }
+    }
+}
