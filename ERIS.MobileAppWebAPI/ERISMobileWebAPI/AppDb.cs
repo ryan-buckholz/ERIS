6114d41301dff9c1428d64c75d34673dda4dc52d
PrestonB404 <94154598+PrestonB404@users.noreply.github.com>
2021-11-29 18:00:05 -0800

    MobileAppWebAPI added. Can query local mock DB.
    MobileAppWebAPI can be consumed with GET command to add a row into a local mock DB.

---
+++ ERIS.MobileAppWebAPI/ERISMobileWebAPI/AppDb.cs
@@ -0,0 +1,20 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERISMobileWebAPI
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
