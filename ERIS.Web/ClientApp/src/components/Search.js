efdbeb648035866b188a3f508d94e6d71a2d6ba7
Ryan Buckholz <rbuckholz1@gmail.com>
2022-02-14 23:10:50 -0800

    Added Search criteria and a place for the results grid

--- ERIS.Web/ClientApp/src/components/Search.js
+++ ERIS.Web/ClientApp/src/components/Search.js
@@ -6,20 +6,66 @@ export class Search extends Component {
     render () {
         return (
             <div>
-                <div className="alert alert-success text-center">Hello, Please enter the search criteria below </div>
-                {/*<div className="row">*/}
-                {/*    <div className="col-md-6 col-xl-4">*/}
-                {/*        <label>Username:</label>*/}
-                {/*        <input className="form-control" type="text" />*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                {/*<div className="row">*/}
-                {/*    <div className="col-md-6 col-xl-4">*/}
-                {/*        <label>Password:</label>*/}
-                {/*        <input className="form-control" type="text" />*/}
-                {/*        <a href="./Search" className="btn btn-primary mt-3 float-end">Login</a>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="alert alert-primary text-center">Hello, Please enter the search criteria below </div>
+                <div className="col-md-12 col-xl-8">
+                    <div className="card bg-light">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-6">
+                                    <label>Date:</label>
+                                    <input className="form-control" type="date" />
+                                </div>
+                                <div className="col-6">
+                                    <label>County:</label>
+                                    <input className="form-control" type="text" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <label>Highway:</label>
+                                    <input className="form-control" type="number" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Mile Marker:</label>
+                                    <input className="form-control" type="number" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <a className="btn btn-primary mt-3 float-end">Search</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card mt-3">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-1">
+                                <label>Date</label>
+                            </div>
+                            <div className="col-2">
+                                <label>County</label>
+                            </div>
+                            <div className="col-2">
+                                <label>Highway</label>
+                            </div>
+                            <div className="col-2">
+                                <label>Mile Marker</label>
+                            </div>
+                            <div className="col-2">
+                                <label>EA</label>
+                            </div>
+                            <div className="col-2">
+                                <label>Project ID</label>
+                            </div>
+                            <div className="col-1">
+                                <label>Select</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
