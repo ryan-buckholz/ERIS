4460527ccfed79b8b73b1cf26004f7d93f0da9af
dudeitstrey <dudeitstrey@yahoo.com>
2022-02-15 22:41:10 -0800

    #121 Added material. Updated flyout menu

---
+++ ERIS.Mobile/ERIS.Mobile/Views/MaterialPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ERIS.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaterialPage : ContentPage
+    {
+        public MaterialPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Rock_CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        private void Bedding_CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        private void Joints_CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        private void Fractures_CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        private void Soil_CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+
+        private void Save_Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Next_Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Back_Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
