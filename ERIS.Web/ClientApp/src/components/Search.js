19671d745123f53e32700091fb734d46f9f7d15f
PrestonB404 <94154598+PrestonB404@users.noreply.github.com>
2022-02-16 20:33:22 -0800

    Added mui packages and Accordion example code to Search page.

--- ERIS.Web/ClientApp/src/components/Search.js
+++ ERIS.Web/ClientApp/src/components/Search.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { SearchResultsGrid } from './SearchResultsGrid';
 import { SearchFilters } from './SearchFilters';
 
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export class Search extends Component {
     static displayName = Search.name;
 
@@ -38,7 +44,45 @@ export class Search extends Component {
                     </div>
                 </div>
 
-
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>Accordion 1</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography>Accordion 2</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion disabled>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3a-content"
+                        id="panel3a-header"
+                    >
+                        <Typography>Disabled Accordion</Typography>
+                    </AccordionSummary>
+                </Accordion>
                 <SearchResultsGrid />
             </div>
         );
