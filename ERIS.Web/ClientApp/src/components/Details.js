762cbb7ea00ffc1f31fe9b3225426b1c6adf224e
jasouneh <jasonfares1016@gmail.com>
2022-02-27 19:19:14 -0800

    -Added link to details page in nav for dev -Added details page

---
+++ ERIS.Web/ClientApp/src/components/Details.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {  Card, CardActions, CardContent} from '@mui/material/';
+
+export class Details extends Component {
+  static displayName  = Details.name;
+  
+  render () {
+    return (
+      <Card variant='outlined'>
+        <CardContent>
+          
+        </CardContent>
+      </Card>
+    )
+  }
+}
