98a5f4518e5171fa91a8515c512ea713a5e379f3
Nataly Kurylov <natalyk@live.com>
2022-03-22 16:43:12 -0700

    198 added card to log in page

--- ERIS.Web/ClientApp/src/components/Home.js
+++ ERIS.Web/ClientApp/src/components/Home.js
@@ -8,7 +8,7 @@ export class Home extends Component {
             <div className="card mt-3">
                 <div className="card-body">
                     <div>
-                        <div className="alert alert-primary text-center col-auto">Hello, Please log in below </div>
+                        <div className="alert alert-primary text-center">Hello, Please log in below </div>
                         <div class="row justify-content-md-center">
                             <div className="col-auto">
                                 <label>Username:</label>
