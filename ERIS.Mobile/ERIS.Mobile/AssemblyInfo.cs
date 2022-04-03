9b098eb0bd2fde8065296cd70531f1d29563d2e5
Giahuy Gonzalez <giahuyg08@gmail.com>
2022-04-02 18:58:38 -0700

    Fixed geo location and prompts theh user that we cannot find the location

--- ERIS.Mobile/ERIS.Mobile/AssemblyInfo.cs
+++ ERIS.Mobile/ERIS.Mobile/AssemblyInfo.cs
@@ -6,4 +6,5 @@ using Xamarin.Forms.Xaml;
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 [assembly: UsesFeature("android.hardware.location", Required = false)]
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
-[assembly: UsesFeature("android.hardware.location.network", Required = false)] 
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+
