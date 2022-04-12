220888e590c768762ce7b3205ff9dc455bae1300
PrestonB404 <94154598+PrestonB404@users.noreply.github.com>
2022-04-11 23:15:38 -0700

    Added missing namespace to DraftsPage.

--- ERIS.Mobile/ERIS.Mobile/Views/DraftsPage.xaml.cs
+++ ERIS.Mobile/ERIS.Mobile/Views/DraftsPage.xaml.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using ERIS.Mobile.ViewModels;
 
 namespace ERIS.Mobile.Views
 {
