0e95b044b84d4b8594cac33b94b8f3508a1c958b
PrestonB404 <94154598+PrestonB404@users.noreply.github.com>
2022-04-30 17:26:32 -0700

    Created AssessmentProfileChangeTracker.

---
+++ ERIS.Mobile/ERIS.Mobile/Services/AssessmentProfileChangeTracker.cs
@@ -0,0 +1,51 @@
+﻿using ERIS.Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ERIS.Mobile.Services
+{
+    public class AssessmentProfileChangeTracker
+    {
+        AssessmentProfileSerializer assessmentProfileSerializer;
+        List<IObserver<AssessmentProfile>> observers;
+        public AssessmentProfileChangeTracker()
+        {
+            assessmentProfileSerializer = DependencyService.Get<AssessmentProfileSerializer>();
+            observers = new List<IObserver<AssessmentProfile>>();
+        }
+
+        public IDisposable Subscribe(IObserver<AssessmentProfile> observer)
+        {
+            if (!observers.Contains(observer))
+                observers.Add(observer);
+
+            return new Unsubscriber(observers, observer);
+        }
+
+        private class Unsubscriber : IDisposable
+        {
+            private List<IObserver<AssessmentProfile>> _observers;
+            private IObserver<AssessmentProfile> _observer;
+
+            public Unsubscriber(List<IObserver<AssessmentProfile>> observers, IObserver<AssessmentProfile> observer)
+            {
+                this._observers = observers;
+                this._observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (!(_observer == null)) _observers.Remove(_observer);
+            }
+        }
+        public void NotifyObservers()
+        {
+            AssessmentProfile assessmentProfile = assessmentProfileSerializer.DeserializeJsonFileToModel();
+
+            foreach (var observer in observers.ToArray())
+                observer.OnNext(assessmentProfile);
+        }
+    }
+}
